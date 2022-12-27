7dfef12903b79f156de234e63a15e9050cc17403
Lasse Sperling <lasse.sperling@tuhh.de>
2022-12-27 21:03:54 +0100

    Now Rooks and Kings can move

--- Chess/chess.py
+++ Chess/chess.py
@@ -1,6 +1,13 @@
 from colors import WHITE, BLACK
 KNIGHT_MOVES = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
-
+KING_MOVES = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+CHESS_BOARD_SIZE = 8
+VERTICAL = 1
+HORIZONTAL = 2
+UP = 1
+DOWN = 2
+LEFT = 3
+RIGHT = 4
 
 class Piece:
     def __init__(self, start_pos, white):
@@ -9,6 +16,69 @@ class Piece:
 
     def move(self, new_pos):
         self.pos = list(new_pos)
+
+    def eliminateInvalidMoves(self, moves):
+        moves = [move for move in moves if (0 <= move[0] <= 7 and 0 <= move[1] <= 7)]
+        # TODO eliminate invalid moves due to check
+        return moves
+
+    def getMovesStraight(self, direction):
+        moves = []
+        num_range = []
+
+        if direction[0] == VERTICAL:
+            if direction[1] == UP:
+                num_range = range(self.pos[0] - 1, -1, -1)
+            elif direction[1] == DOWN:
+                num_range = range(self.pos[0] + 1, CHESS_BOARD_SIZE)
+
+            for i in num_range:
+                if board.board[i][self.pos[1]] is not None:
+                    piece = board.board[i][self.pos[1]]
+                    if piece.color == self.color:
+                        break
+                    else:
+                        moves.append([i, self.pos[1]])
+                        break
+                moves.append([i, self.pos[1]])
+
+        elif direction == HORIZONTAL:
+            if direction[1] == LEFT:
+                num_range = range(self.pos[1] - 1, -1, -1)
+            elif direction[1] == RIGHT:
+                num_range = range(self.pos[1] + 1, CHESS_BOARD_SIZE)
+
+            for i in num_range:
+                if board.board[self.pos[0]][i] is not None:
+                    piece = board.board[self.pos[0]][i]
+                    if piece.color == self.color:
+                        break
+                    else:
+                        moves.append([self.pos[0], i])
+                        break
+                moves.append([self.pos[0], i])
+
+        return moves
+
+    def getAllMovesStraight(self):
+        moves_down = self.getMovesStraight((VERTICAL, DOWN))
+        moves_up = self.getMovesStraight((VERTICAL, UP))
+        moves_left = self.getMovesStraight((HORIZONTAL, LEFT))
+        moves_right = self.getMovesStraight((HORIZONTAL, RIGHT))
+        moves = moves_down + moves_up + moves_left + moves_right
+        return moves
+
+    def getMovesDiagonal(self, direction):
+        moves = []
+        return moves
+
+    def getAllMovesDiagonal(self):
+        moves_up_right = self.getMovesDiagonal((UP, RIGHT))
+        moves_up_left = self.getMovesDiagonal((UP, LEFT))
+        moves_down_left = self.getMovesDiagonal((DOWN, LEFT))
+        moves_down_right = self.getMovesDiagonal((DOWN, RIGHT))
+        moves = moves_up_right + moves_up_left + moves_down_left + moves_down_right
+        return moves
 
 
 class Pawn(Piece):
@@ -22,6 +92,10 @@ class Pawn(Piece):
 class Rook(Piece):
     def __repr__(self):
         return f"Rook at ({self.pos[0]}, {self.pos[1]})"
+    
+    def getPossibleMoves(self):
+        moves = self.getAllMovesStraight()
+        return self.eliminateInvalidMoves(moves)
 
 
 class Knight(Piece):
@@ -29,25 +103,37 @@ class Knight(Piece):
         return f"Knight at ({self.pos[0]}, {self.pos[1]})"
 
     def getPossibleMoves(self):
-        # TODO eliminate invalid moves due to check
         moves = [[self.pos[i] + move[i] for i in range(2)] for move in KNIGHT_MOVES]
-        moves = [move for move in moves if (0 <= move[0] <= 7 and 0 <= move[1] <= 7)]
-        return moves
+        return self.eliminateInvalidMoves(moves)
 
 
 class Bishop(Piece):
     def __repr__(self):
         return f"Bishop at ({self.pos[0]}, {self.pos[1]})"
 
+    def getPossibleMoves(self):
+        moves = self.getAllMovesDiagonal()
+        return self.eliminateInvalidMoves(moves)
+
 
 class Queen(Piece):
     def __repr__(self):
         return f"Queen at ({self.pos[0]}, {self.pos[1]})"
 
+    def getPossibleMoves(self):
+        straight_moves = self.getAllMovesStraight()
+        diagonal_moves = self.getAllMovesDiagonal()
+        moves = diagonal_moves + straight_moves
+        return self.eliminateInvalidMoves(moves)
+
 
 class King(Piece):
     def __repr__(self):
         return f"King at ({self.pos[0]}, {self.pos[1]})"
+
+    def getPossibleMoves(self):
+        moves = [[self.pos[i] + move[i] for i in range(2)] for move in KING_MOVES]
+        return self.eliminateInvalidMoves(moves)
 
 
 class Board:
@@ -60,12 +146,12 @@ class Board:
                 Queen([0, 3], white), King([0, 4], white), Bishop([0, 5], white),
                 Knight([0, 6], white), Rook([0, 7], white)
             ],
-            [Pawn([1, i], white) for i in range(8)],
-            [None for _ in range(8)],
-            [None for _ in range(8)],
-            [None for _ in range(8)],
-            [None for _ in range(8)],
-            [Pawn([6, i], bool(1-white)) for i in range(8)],
+            [Pawn([1, i], white) for i in range(CHESS_BOARD_SIZE)],
+            [None for _ in range(CHESS_BOARD_SIZE)],
+            [None for _ in range(CHESS_BOARD_SIZE)],
+            [None for _ in range(CHESS_BOARD_SIZE)],
+            [None for _ in range(CHESS_BOARD_SIZE)],
+            [Pawn([6, i], bool(1-white)) for i in range(CHESS_BOARD_SIZE)],
             [
                 Rook([7, 0], bool(1-white)), Knight([7, 1], bool(1-white)), Bishop([7, 2], bool(1-white)),
                 Queen([7, 3], bool(1-white)), King([7, 4], bool(1-white)), Bishop([7, 5], bool(1-white)),
